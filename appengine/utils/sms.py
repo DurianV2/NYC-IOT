7c350efb2dd50e874f6836e74b7bbb21be56c3bc
Innocent Niyibizi <25cent9@gmail.com>
2018-07-21 06:17:37 -0400

    Update sms.py

--- appengine/utils/sms.py
+++ appengine/utils/sms.py
@@ -5,8 +5,8 @@ class SMS:
         self.text_message = text_message
 
     def send_text(self):
-        account_sid = "test-sid"
-        auth_token = "test-token"
+        account_sid = "ACac28365d30b3d23f537ee519361e07b2"
+        auth_token = "81897784866648c1ecca082cf24638e3"
         fromnumber = "+12248033227"
         tonumber = "+17733296548",
         client = Client(account_sid, auth_token)
