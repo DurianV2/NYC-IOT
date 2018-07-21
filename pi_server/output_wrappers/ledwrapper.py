66c6847a9e59465271ea1ef39847cc8d761f7fcd
25cent9 <25cent9@gmail.com>
2018-07-21 06:40:39 -0400

    Fixes comedic logic errors

--- pi_server/output_wrappers/ledwrapper.py
+++ pi_server/output_wrappers/ledwrapper.py
@@ -6,16 +6,16 @@ class LedWrapper:
     red_pin = None
 
     def __init__(self, green_pin, red_pin):
-        GPIO.setmode(GPIO.BOARD)
+        GPIO.setmode(GPIO.BCM)
         self.green_pin = green_pin
         self.red_pin = red_pin
 
     def set_green(self, turningOn):
-        GPIO.setmode(GPIO.BOARD)
+        GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.green_pin, GPIO.OUT)
-        GPIO.output(self.green_pin, turningOn)
+        GPIO.output(self.green_pin, not turningOn)
 
     def set_red(self, turningOn):
-        GPIO.setmode(GPIO.BOARD)
+        GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.red_pin, GPIO.OUT)
-        GPIO.output(self.red_pin, turningOn)
+        GPIO.output(self.red_pin, not turningOn)
