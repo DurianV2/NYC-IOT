01f79895b342859964bf6352a257ee8b8d99b60a
Innocent Niyibizi <25cent9@gmail.com>
2018-07-21 07:43:15 -0400

    Update app_utils.py

--- appengine/app_utils.py
+++ appengine/app_utils.py
@@ -1,12 +1,2 @@
-from utils.sms import SMS
-
-def send_second_level_sms(host, contacts):
-    message_handler = SMS(host + " has not returned home")
-    for contact in contacts:
-        message_handler.send_text()
-
-def send_third_level_sms(host):
-    message_handler = SMS(host + " has not returned home! Please help")
-    message_handler.send_text()
-
+# Utils for the app
 
